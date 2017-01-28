bd40def1416edc696fc6dbcc9c76c6530168b477
bit49021 <somniator9999@gmail.com>
2017-01-27 17:40:08 -0700

    urls and NewsArticle model

--- siteportal/news/models.py
+++ siteportal/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class NewsArticle(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=10000)
+    pub_date = models.DateTimeField('date published')
+    category = models.CharField(max_length=30)
