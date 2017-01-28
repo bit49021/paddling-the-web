f8c1ef962f75b2f314380c910689b489d9bca3fe
bit49021 <somniator9999@gmail.com>
2017-01-27 18:56:00 -0700

    basic news templates

--- siteportal/news/urls.py
+++ siteportal/news/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^articles/(?P<article_id>[0-9]+)/$', views.article_detail, name="article_detail"),
 ]
--- siteportal/news/views.py
+++ siteportal/news/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import NewsArticle
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is the news section.")
+    all_articles_result = NewsArticle.objects.all()
+
+    context = {}
+    context['all_articles'] = all_articles_result
+
+    return render(request, 'news/index.html', context)
+
+def article_detail(request, article_id):
+    article = get_object_or_404(NewsArticle, id=article_id)
+    return render(request, 'news/detail.html', {'article' : article})
